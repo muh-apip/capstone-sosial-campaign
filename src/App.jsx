3dea2ac2faeeff73eff33b51a25a062c9725cbcb
Ajeng Nursyifa <ajengnursyifa18@gmail.com>
2024-11-28 10:17:27 +0700

    ajeng-dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import Navbar from './components/Navbar'
-import HeroSection from './components/HeroSection'
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import HeroSection from "./pages/HeroSection";
+import AboutUs from "./pages/AboutUs";
+import Fitur from "./pages/Fitur";
+import FAQ from "./pages/FAQ";
 
 function App() {
-
   return (
     <>
-     <Navbar />
-     <HeroSection/>
+      <Navbar />
+      <HeroSection />
+      <AboutUs />
+      <Fitur />
+      <FAQ />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
