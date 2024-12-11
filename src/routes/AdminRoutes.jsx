2bdd6aa382c65e37a486b2e16a496e9af9e83ee9
Joenathan <joenathandaniel33@gmail.com>
2024-12-11 22:48:07 +0700

    Clients

--- src/routes/AdminRoutes.jsx
+++ src/routes/AdminRoutes.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import KegiatanRelawanAdmin from "../components/Admin/KegiatanRelawan/KegiatanRelawanAdmin";
 import TambahKegiatanAdmin from "../components/Admin/KegiatanRelawan/TambahKegiatanAdmin";
 import EditKegiatanAdmin from "../components/Admin/KegiatanRelawan/EditKegiatanAdmin";
+import TableClient from "../components/Admin/Client/ClientsAdmin";
 const PrivateRoutes = () => {
   return (
     <Routes>
       <Route path="/kegiatan" element={<KegiatanRelawanAdmin />} />
       <Route path="/kegiatan/tambah" element={<TambahKegiatanAdmin />} />
       <Route path="/kegiatan/edit/:id" element={<EditKegiatanAdmin />} />
+      <Route path="/client" element={<TableClient />} />
     </Routes>
   );
 };
