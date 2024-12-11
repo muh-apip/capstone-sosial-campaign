083ddbefd6eb2673414789ff1ddfb9ffcfa6e506
Muhammad Haviv <75481735+muh-apip@users.noreply.github.com>
2024-12-11 23:07:49 +0700

    Update AdminRoutes.jsx

--- src/routes/AdminRoutes.jsx
+++ src/routes/AdminRoutes.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import KegiatanrelawanAdmin from "../components/Admin/KegiatanRelawan/KegiatanrelawanAdmin";
+import KegiatanRelawanAdmin from "../components/Admin/KegiatanRelawan/KegiatanRelawanAdmin";
 import TambahKegiatanAdmin from "../components/Admin/KegiatanRelawan/TambahKegiatanAdmin";
 import EditKegiatanAdmin from "../components/Admin/KegiatanRelawan/EditKegiatanAdmin";
 import DashboardAdmin from "../components/Admin/Dashboard/DashboardAdmin";
@@ -17,7 +17,7 @@ const AdminRoutes = () => {
   return (
     <Routes>
       <Route path="/dashboard" element={<DashboardAdmin />} />
-      <Route path="/relawan-admin" element={<KegiatanrelawanAdmin />} />
+      <Route path="/relawan-admin" element={<KegiatanRelawanAdmin />} />
       <Route path="/relawan/tambah" element={<TambahKegiatanAdmin />} />
       <Route path="/relawan/edit/:id" element={<EditKegiatanAdmin />} />
       <Route path="/donasi-admin" element={<DonasiAdmin />} />
