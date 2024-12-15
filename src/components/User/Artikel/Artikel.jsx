a5e45eabb80fe833913aaccb1f18678ff9e8a077
Ajeng Nursyifa <ajengnursyifa18@gmail.com>
2024-12-15 21:41:48 +0700

    Update Artikel.jsx

--- src/components/User/Artikel/Artikel.jsx
+++ src/components/User/Artikel/Artikel.jsx
@@ -140,23 +140,6 @@ const Artikel = () => {
               )
             )}
           </div>
-
-          {/* Pagination */}
-          <div className="flex justify-center mt-6 space-x-2">
-            {Array.from({ length: totalPages }).map((_, index) => (
-              <button
-                key={index}
-                onClick={() => handlePageChange(index + 1)}
-                className={`px-4 py-2 rounded-lg shadow ${
-                  currentPage === index + 1
-                    ? "bg-custom-green text-white"
-                    : "bg-gray-300 text-gray-600"
-                }`}
-              >
-                {index + 1}
-              </button>
-            ))}
-          </div>
         </div>
       </div>
 
